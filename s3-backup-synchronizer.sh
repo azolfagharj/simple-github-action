d5f448473d184aee59243f6cb2eeccfca2393f1f
Alireza zolfaghar <azolfagharj@gmail.com>
2025-02-21 14:20:16 +0330

    end of workflow
    All done

---
+++ s3-backup-synchronizer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Simple Bash script to show how github action work
+AUTHOR="ALIREZA.ZOLFAGHAR"
+VERSION="2.9.0"
+VERSION_UPDATED="2025-02-21"
+UPLOAD_TO_ALIREZAZ=true
---
+++ simple-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Simple Bash script to show how github action work
+AUTHOR="ALIREZA.ZOLFAGHAR"
+VERSION="2.9.0"
+VERSION_UPDATED="2025-02-21"
+UPLOAD_TO_ALIREZAZ=true
+echo "Hello World"
+echo "Author: $AUTHOR"
+echo "Version: $VERSION"
+echo "Updated: $VERSION_UPDATED"
+echo "Upload to Alirezaz: $UPLOAD_TO_ALIREZAZ"
+echo "If true, it will upload to External Server"
+echo "End of Script"
