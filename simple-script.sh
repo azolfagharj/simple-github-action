42f25c7222c03134afcd199d4f29eecc53edc09d
Alireza zolfaghar <azolfagharj@gmail.com>
2025-02-21 18:47:04 +0330

    update pro version
    Add pro Version of workflow

--- simple-script.sh
+++ simple-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Simple Bash script to show how github action work
 AUTHOR="ALIREZA.ZOLFAGHAR"
-VERSION="2.9.0"
+VERSION="2.9.1"
 VERSION_UPDATED="2025-02-21"
 UPLOAD_TO_ALIREZAZ=true
 echo "Hello World"
